81d6ebaae7eb4c1e57b95b5ae500b950f987a36c
Dhananjay Balan <mail@dbalan.in>
2025-04-01 10:46:05 +0200

    Add support for more attributes
    1. Added support for time for testcase and testsuites.
    2. Added support for skipped testcases

--- junitxml.go
+++ junitxml.go
@@ -1,44 +1,66 @@
 /*
-   Copyright 2021 Marty Pauley
+Copyright 2021 Marty Pauley
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 package junitxml
 
 import (
 	"encoding/xml"
 	"io"
+	"time"
 )
 
 type JUnitXML struct {
-	XMLName xml.Name     `xml:"testsuites"`
-	Suites  []*TestSuite `xml:"testsuite"`
+	XMLName      xml.Name     `xml:"testsuites"`
+	Suites       []*TestSuite `xml:"testsuite"`
+	TestCount    int          `xml:"tests,attr"`
+	FailureCount int          `xml:"failures,attr"`
+	Time         string       `xml:"time,attr"`
 }
+
 type TestSuite struct {
 	Name         string      `xml:"name,attr"`
 	TestCount    int         `xml:"tests,attr"`
 	FailureCount int         `xml:"failures,attr"`
+	SkippedCount int         `xml:"skipped,attr,omitempty"`
+	Time         string      `xml:"time,attr"`
+	Timestamp    string      `xml:"timestamp,attr"`
 	ErrorCount   int         `xml:"errors,attr"`
 	Cases        []*TestCase `xml:"testcase"`
 }
+
 type TestCase struct {
-	Name     string   `xml:"name,attr"`
-	Failures []string `xml:"failure,omitempty"`
-	Error    string   `xml:"error,omitempty"`
+	Name        string     `xml:"name,attr"`
+	Time        string     `xml:"time,attr"`
+	Classname   string     `xml:"classname,attr"`
+	Group       string     `xml:"group,attr"`
+	Failures    []*Message `xml:"failure,omitempty"`
+	Error       *Message   `xml:"error,omitempty"`
+	SkipMessage *Message   `xml:"skipped,omitempty"`
+}
+
+type Message struct {
+	Message string `xml:"message,attr"`
 }
 
 func (j *JUnitXML) Suite(name string) *TestSuite {
-	ts := &TestSuite{Name: name}
+	// get unix timestamp
+	var (
+		currentTime = time.Now().Format(time.RFC3339)
+	)
+
+	ts := &TestSuite{Name: name, Timestamp: currentTime}
 	j.Suites = append(j.Suites, ts)
 	return ts
 }
@@ -61,14 +83,22 @@ func (ts *TestSuite) lastCase() *TestCase {
 	return ts.Cases[len(ts.Cases)-1]
 }
 
-func (ts *TestSuite) Fail(f string) {
+func (ts *TestSuite) Fail(msg, tme string) {
 	ts.FailureCount++ // yes, there can be more failures than test cases
 	curt := ts.lastCase()
-	curt.Failures = append(curt.Failures, f)
+	curt.Failures = append(curt.Failures, &Message{msg})
+	curt.Time = tme
 }
 
-func (ts *TestSuite) Abort(e error) {
+func (ts *TestSuite) Abort(e error, tme string) {
 	ts.ErrorCount++
 	curt := ts.lastCase()
-	curt.Error = e.Error()
+	curt.Error = &Message{e.Error()}
+	curt.Time = tme
+}
+
+func (ts *TestSuite) Skip(msg string) {
+	curt := ts.lastCase()
+	curt.SkipMessage = &Message{msg}
+	curt.Time = "0.0"
 }
--- junitxml_test.go
+++ junitxml_test.go
@@ -27,8 +27,8 @@ func TestJunitOutput(t *testing.T) {
 		t.Errorf("JUnit output had %d testsuite elements; expected 3\n", len(suites))
 	}
 	cases := reCases.FindAll(output, -1)
-	if len(cases) != 7 {
-		t.Errorf("JUnit output had %d testcase; expected 7\n", len(cases))
+	if len(cases) != 8 {
+		t.Errorf("JUnit output had %d testcase; expected 8\n", len(cases))
 	}
 }
 
@@ -41,9 +41,12 @@ func FakeTestSuites() *JUnitXML {
 	mixed := ju.Suite("mixed")
 	mixed.Case("good")
 	bad := mixed.Case("bad")
-	bad.Fail("once")
-	bad.Fail("twice")
-	mixed.Case("ugly").Abort(errors.New("buggy"))
-	ju.Suite("fast").Fail("fail early")
+	bad.Fail("once", "0.01000")
+	bad.Fail("twice", "0.01000")
+	mixed.Case("ugly").Abort(errors.New("buggy"), "0.1000")
+	ju.Suite("fast").Fail("fail early", "0.0")
+	skipped := mixed.Case("skipped")
+	skipped.Skip("skipped")
+
 	return ju
 }
